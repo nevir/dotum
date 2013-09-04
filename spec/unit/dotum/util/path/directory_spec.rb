8588582bf6e288e61b42649075bd4185e01daab6
Ian MacLeod <ian@nevir.net>
2013-09-03 20:31:47 -0700

    Aaand more

---
+++ spec/unit/dotum/util/path/directory_spec.rb
@@ -0,0 +1,22 @@
+# encoding: utf-8
+
+describe Dotum::Util::Path, "#directory?" do
+  include_context "path fixtures"
+
+  it "should be false for files" do
+    expect(described_class.new(file_fixture).directory?).to be_false
+  end
+
+  it "should be true for directories" do
+    expect(described_class.new(directory_fixture).directory?).to be_true
+  end
+
+  it "should be false for symlinks" do
+    expect(described_class.new(symlink_fixture).directory?).to be_false
+  end
+
+  it "should be false for non-entities" do
+    expect(described_class.new(non_fixture).directory?).to be_false
+  end
+
+end
--- spec/unit/dotum/util/path/exists_spec.rb
+++ spec/unit/dotum/util/path/exists_spec.rb
@@ -1,17 +1,22 @@
 # encoding: utf-8
 
 describe Dotum::Util::Path, "#exists?" do
+  include_context "path fixtures"
 
   it "should be true for files" do
-    expect(described_class.new(__FILE__)).to exist
+    expect(described_class.new(file_fixture).exists?).to be_true
   end
 
   it "should be true for directories" do
-    expect(described_class.new(File.dirname(__FILE__))).to exist
+    expect(described_class.new(directory_fixture).exists?).to be_true
+  end
+
+  it "should be true for symlinks" do
+    expect(described_class.new(symlink_fixture).exists?).to be_true
   end
 
   it "should be false for non-entities" do
-    expect(described_class.new(File.join(__FILE__, "nope"))).to_not exist
+    expect(described_class.new(non_fixture).exists?).to be_false
   end
 
 end
