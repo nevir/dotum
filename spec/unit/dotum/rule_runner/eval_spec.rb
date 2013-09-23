4844b33fe93585decae398371c9537ff40543f72
Ian MacLeod <ian@nevir.net>
2013-09-22 19:40:55 -0700

    RuleRunner specs

---
+++ spec/unit/dotum/rule_runner/eval_spec.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+
+describe Dotum::RuleRunner, ".eval" do
+
+  it "should return the result of the eval'd block" do
+    expect(described_class.eval(nil) { 123 }).to eq(123)
+  end
+
+  it "should run the block in an instance of its self" do
+    obj = described_class.eval(nil) { self }
+    expect(obj).to be_a(described_class)
+  end
+
+  it "should set the context" do
+    expect(described_class.eval(:context) { context }).to eq(:context)
+  end
+
+  it "should support string-based eval, too" do
+    begin
+      described_class.eval(nil, "raise 'foo'", "FILE", 123)
+    rescue RuntimeError => error
+      expect(error.backtrace.first).to eq("FILE:123:in `eval'")
+    end
+  end
+
+end
---
+++ spec/unit/dotum/rule_runner_spec.rb
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+describe Dotum::RuleRunner do
+
+  it "should include the rule DSL" do
+    expect(described_class.included_modules).to include(Dotum::RuleDSL)
+  end
+
+end
